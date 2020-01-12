c0a9bbd55cf99703bf0e6b681e38d0c0fccef8f6
A.Sikorski <sikorski.aps@gmail.com>
2020-01-12 19:31:42 +0100

    First working version of the interpreter with execution! :)

--- src/modules/Shell.cpp
+++ src/modules/Shell.cpp
@@ -3,15 +3,22 @@
 #include <iostream>
 #include <stdexcept>
 #include <string>
+#include "../parser.hpp"
 
 void Shell::start() {
     Context context;
     while(context.isRunning()){
         std::string input;
+        std::cout << "$" << context.getWorkingDirectory() << " ";
         std::getline(std::cin, input);
-
+        Program * program = parse(input);
         // parse input and start execution
-        throw std::runtime_error("TODO: Shell::start");
+        if(program){
+            std::cout << input;
+//            std::cerr << "Starting program..." << std::endl;
+            std::cout << program->execute(&context) << std::endl;
+        }
+//        throw std::runtime_error("TODO: Shell::start");
     }
     std::cout << "Exiting...\n";
 }
