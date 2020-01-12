d8b228da6ce264518e2947dd95e5ec04e59a9ff3
muffinangel <bilewicz.kar@gmail.com>
2020-01-12 13:49:49 +0100

    Added Variable Execution

--- src/structures/Variable.cpp
+++ src/structures/Variable.cpp
@@ -2,8 +2,13 @@
 #include <stdexcept>
 #include "Variable.h"
 
-std::string Variable::execute(Context *) {
-    throw std::runtime_error("TODO: Variable::execute");
+std::string Variable::execute(Context *c) {
+    std::string name = this->name.execute(c);
+    std::string newValue = c->resolveVariables(this->newValue.execute(c));
+
+    c->setVariable(name, newValue);
+
+    return "";
 }
 
 Variable::Variable(Value const &name, Value const &newValue, bool shouldExport)
