2af052e3a7075b82e1970eff5b0bebd4a9aaafde
A.Sikorski <sikorski.aps@gmail.com>
2020-01-12 16:52:49 +0100

    Nice TestMain.cpp

--- tests/TestMain.cpp
+++ tests/TestMain.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 int main() {
+    // Working examples
     Program pwd = generateSimplePwd();
     Program cd = generateSimpleCd();
     Program echo = generateSimpleEcho();
@@ -19,42 +20,17 @@ int main() {
     Program redirection = generateBothWayRedirection();
     Program simpleBackTickPwd = generateSimpleBackTick();
 
-
     // Errors
     Program assignmentFailed = variableAssignementFail();
     Program assignmentFailedEchoSuccess = variableAssignementFailedBeforeSuccessfulInstr();
     Program assignmentFailedEchoFailed = failedVariableAssignementEchoed();
     Program cdFailed = cdToNonExistingFolder();
 
-//     auto ex = Executor();
-//     ex.execute(assignment);
-
-// // for testing assigning variable call to another variable TODO make test for it
-//     // std::string variableName = "local2";
-//     // VariableCall vCall = VariableCall("local");
-//     // VariableAssignment varAssignment = VariableAssignment(variableName, &vCall);
-//     // std::vector<Node*> commands;
-//     // commands.push_back(&varAssignment);
-//     // Program p = Program(commands);
-//     // ex.execute(p);
+    // Example execution here (write your tests here)
+    Context context;
 
 
-//     Cleaner cleaner = Cleaner();
-//     cleaner.clean(&pwd);
-//     cleaner.clean(&cd);
-//     cleaner.clean(&echo);
-//     cleaner.clean(&echo2);
-//     cleaner.clean(&echo3);
-//     cleaner.clean(&assignment);
-//     cleaner.clean(&exported);
-//     cleaner.clean(&pipe1);
-//     cleaner.clean(&pipe2);
-//     cleaner.clean(&pipe3);
-//     cleaner.clean(&outRedirection);
-//     cleaner.clean(&inRedirection);
-//     cleaner.clean(&redirection);
-
-    // Context tests
+    // Automatic Context tests
     testContextNoReplecement();
     testContextNormalReplacements();
     testEscapedDollarInText();
