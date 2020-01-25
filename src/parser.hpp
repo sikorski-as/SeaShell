36bcc444cba89f3bd4aeae9fe4978ce8c500611d
Andrei Shumak <shakandrew1996@gmail.com>
2020-01-25 21:55:13 +0100

    comment debug statements

--- src/parser.hpp
+++ src/parser.hpp
@@ -1,8 +1,9 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.5.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015, 2018-2019 Free Software Foundation,
+   Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -30,6 +31,9 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
+/* Undocumented macros, especially those whose name start with YY_,
+   are private implementation details.  Do not rely on them.  */
+
 #ifndef YY_YY_PARSER_HPP_INCLUDED
 # define YY_YY_PARSER_HPP_INCLUDED
 /* Debug traces.  */
@@ -40,7 +44,7 @@
 extern int yydebug;
 #endif
 /* "%code requires" blocks.  */
-#line 1 "parser.ypp" /* yacc.c:1909  */
+#line 1 "parser.ypp"
 
   #include "structures/Program.h"
   #include "structures/Pipeline.h"
@@ -53,10 +57,10 @@ extern int yydebug;
   #include <iostream>
   #include <exception>
   #include <string>
-  #define DEBUG
+  // #define DEBUG
   #define YYERROR_VERBOSE 1
   using namespace std;
-extern int yylex_destroy(void);
+  extern int yylex_destroy(void);
   int yylex(void);
   void set_input_string(const char* in);
   void end_lexical_scan(void);
@@ -65,7 +69,7 @@ extern int yylex_destroy(void);
   void parse_to_root(std::string);
   void find_bt_expressions(std::string);
 
-#line 69 "parser.hpp" /* yacc.c:1909  */
+#line 73 "parser.hpp"
 
 /* Token type.  */
 #ifndef YYTOKENTYPE
@@ -92,10 +96,9 @@ extern int yylex_destroy(void);
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
 union YYSTYPE
 {
-#line 36 "parser.ypp" /* yacc.c:1909  */
+#line 36 "parser.ypp"
 
     char* sval;
     Command *command;
@@ -106,9 +109,9 @@ union YYSTYPE
     Variable *variable;
     VarPip *varpip;
 
-#line 110 "parser.hpp" /* yacc.c:1909  */
-};
+#line 113 "parser.hpp"
 
+};
 typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
